99be1a72ae18f27905f8cc7f3729980e13861e8f
Hiren Gohil <50377062+driveofhiren@users.noreply.github.com>
2024-01-21 16:04:40 -0500

    Add files via upload

---
+++ Hiren_BookStore1/Products.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="Hiren_BookStore.Products" MasterPageFile="~/Site.master" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+       <div class="row thumbnail1">
+           <div class="row"><%-- row 1 --%>
+                <div class="col-sm-12">
+    <asp:Label ID="lblWelcome" runat="server" CssClass="text-capitalize"></asp:Label>
+</div>
+        <asp:Repeater ID="rptProducts" runat="server"  DataSourceID="SqlDataSource">
+            <ItemTemplate>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="thumbnail">
+                        <img src='Images/<%# Eval("ImageFile") %>'
+                            alt='<%# Eval("Name") %>' width="100" height="100" />
+                        <div class="caption">
+                            <h3><%# Eval("Name") %></h3>
+                            <p>
+                                <b>Price: <%# Eval("UnitPrice", "{0:0.00}") %></b>
+                                <br>
+                                <br>
+                                <%# Eval("LongDescription") %>
+                            </p>
+                           <asp:Button ID="btnAdd" runat="server" 
+                            CommandArgument='<%# Eval("ProductID") %>'  OnClick="btnAdd_Click" CssClass="btn" Text="Add to Cart" />
+                        </div>
+                                              
+                    </div>    
+                </div>
+            </ItemTemplate>
+
+        </asp:Repeater>
+    </div>
+    <asp:SqlDataSource ID="SqlDataSource" runat="server" 
+    ConnectionString='<%$ ConnectionStrings:Hiren_BookStore %>' 
+    SelectCommand="SELECT [ProductID], [Name], [ShortDescription], [LongDescription], [ImageFile], [UnitPrice] FROM [Books] ORDER BY [Name]">
+</asp:SqlDataSource>
+
+    </asp:Content>
---
+++ Hiren_BookStore1/Products.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hiren_BookStore
+{
+    public partial class Products : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            rptProducts.DataSourceID = "SqlDataSource";
+            HttpCookie firstName = Request.Cookies["FirstName"];
+            if (firstName != null)
+                lblWelcome.Text = "<h4>welcome back, " + firstName.Value + "!</h4>";
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                Button button = (Button)sender;
+                //get cart from session and selected item from cart
+                CartItemList cart = CartItemList.GetCart();
+               CartItem cartItem = cart[button.CommandArgument.ToString()];
+                Product selectedBook = GetSelectedProduct(button.CommandArgument.ToString());
+
+                //if item isn’t in cart, add it; otherwise, increase its quantity
+                if (cartItem == null)
+                {
+                    cart.AddItem(selectedBook, 1);
+                }
+                else
+                {
+                    cartItem.AddQuantity(1);
+                }
+                Response.Redirect("Cart.aspx", false);
+                rptProducts.DataBind();
+            }
+        }
+
+        private Product GetSelectedProduct(String ProductID)
+        {
+            DataView booksTable = (DataView)
+                SqlDataSource.Select(DataSourceSelectArguments.Empty);
+            booksTable.RowFilter =
+                "ProductID = '" + ProductID + "'";
+            DataRowView row = booksTable[0];
+
+            //create a new book object and load with data from row
+            Product b = new Product();
+            b.ProductID = row["ProductID"].ToString();
+            b.Name = row["Name"].ToString();
+            b.ShortDescription = row["ShortDescription"].ToString();
+            b.LongDescription = row["LongDescription"].ToString();
+            b.UnitPrice = (decimal)row["UnitPrice"];
+            b.ImageFile = row["ImageFile"].ToString();
+            return b;
+        }
+
+    }
+}
